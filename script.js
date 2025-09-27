f2b39f274e7679b9d3962411b767e930efb31398
niels31 <118722678+niels31@users.noreply.github.com>
2025-09-27 19:36:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var map=L.map("map", {center:[-34.83648874965474, -58.097171123565104], zoom:14});
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'jpg'
+var map=L.map("map", {center:[-34.83648874965474, -58.097171123565104], zoom:12});
+L.tileLayer('https://data.geopf.fr/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', {
+	attribution: '<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>',
+	bounds: [[-75, -180], [81, 180]],
+	minZoom: 2,
+	maxZoom: 12,
+	format: 'image/jpeg',
+	style: 'normal'
 }).addTo(map);
 
 function addGeoJSON(geojson){
